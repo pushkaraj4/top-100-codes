9d5cfaa71cffba56149dda3f570c36a0456e3e04
Pushkaraj <pushkarajy4@gmail.com>
2024-09-07 22:37:24 +0530

    Count the number of vowels in the string

--- CountVowels.java
+++ CountVowels.java
@@ -4,6 +4,20 @@ public class CountVowels {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        
+        String str = sc.nextLine();
+        int count = countVowels(str);
+        System.out.println(count);
+        sc.close();
+    }
+
+    static int countVowels(String str) {
+        int cnt = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char lowercase = Character.toLowerCase(str.charAt(i));
+            if (lowercase == 'a' || lowercase == 'e' || lowercase == 'i' || lowercase == 'o' || lowercase == 'u') {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
--- StrLength.java
+++ StrLength.java
@@ -1,4 +1,5 @@
 public class StrLength {
+    @SuppressWarnings("all")
     public static void main(String[] args) {
         String str = "Pushkaraj";
         int len = 0;
